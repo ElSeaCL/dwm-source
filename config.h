ce71d6b6b0e723b4ecc49c8d79a80027bc2a859f
seba.pi4 <gonzalez.gatica@gmail.com>
2020-11-26 23:31:29 -0300

    Se cambia el tipo de letra del terminal por Hack

--- config.h
+++ config.h
@@ -5,9 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
 
+/*
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+*/
+
+static char *font = "Hack:style=Regular:pixelsize=14:antialias=true:autohint=true";
+static int borderpx = 2;
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
